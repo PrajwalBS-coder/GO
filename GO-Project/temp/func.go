668702ee7485cecb309cb158baa1f1546569dffa
PrajwalBS-coder <prajwaljhon@gmail.com>
2025-07-01 15:35:50 +0530

    Changes in Function

--- GO-Project/temp/func.go
+++ GO-Project/temp/func.go
@@ -6,6 +6,10 @@ import (
 )
 
 func Greet(name string)(string, error) {
-	fmt.Println("Hello, World!", name)
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	msg:=fmt.Sprintf("Hello, %v World!", name)
+	return  msg,nil
 	// fmt.Println("This is a temporary package.")
 }
