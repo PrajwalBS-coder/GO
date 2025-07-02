34fe78b9412c75aaaae9423d6f6d9f34622c9029
PrajwalBS-coder <prajwaljhon@gmail.com>
2025-07-02 22:18:34 +0530

    Giving Random Welcome texts Using math module rand function

--- GO-Project/temp/func.go
+++ GO-Project/temp/func.go
@@ -3,13 +3,22 @@ package temp
 import (
 	"fmt"
 	"errors"
+	"math/rand"
 )
 
 func Greet(name string)(string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	msg:=fmt.Sprintf("Hello, %v World!", name)
+	msg:=fmt.Sprintf(randomFormat(), name)
 	return  msg,nil
-	// fmt.Println("This is a temporary package.")
+}
+func randomFormat() string {
+    // A slice of message formats.
+    formats := []string{
+        "Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+    }
+	return formats[rand.Intn(len(formats))]
 }
