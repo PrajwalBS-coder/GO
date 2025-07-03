d8e1e7dadaf1ad94a79bee93ef999761f21b07bb
PrajwalBS-coder <prajwaljhon@gmail.com>
2025-07-03 20:57:41 +0530

    New Function

--- GO-Project/temp/func.go
+++ GO-Project/temp/func.go
@@ -6,13 +6,33 @@ import (
 	"math/rand"
 )
 
-func Greet(name string)(string, error) {
-	if name == "" {
-		return "", errors.New("empty name")
-	}
-	msg:=fmt.Sprintf(randomFormat(), name)
-	return  msg,nil
+// func Greet(name [] string)([]string, error) {
+// 	var a []string
+// 	for _, n := range name{
+// 		if n ==''{
+// 			return nil errors.New("empty name")
+// 		}
+		
+// 		msg:=fmt.Sprintf(randomFormat(), name)
+// 		a = append(a, msg)
+
+// 	}
+// return a,nil
+// }
+func Greet(names []string) ([]string, error) {
+    var greetings []string
+    for _, name := range names {
+        if name == "" {
+            return nil, errors.New("empty name")
+        }
+        msg := fmt.Sprintf(randomFormat(), name)
+        greetings = append(greetings, msg)
+    }
+    return greetings, nil
 }
+
+	
+// }
 func randomFormat() string {
     // A slice of message formats.
     formats := []string{
