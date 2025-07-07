cf4bc33171c969271fe4853601bacc787c833e2c
Prajwal B S <prajwaljhon@gmail.com>
2025-07-07 21:14:30 +0530

    Add byte variable declaration and type reflection in int.go

--- GO-Project/basics/int.go
+++ GO-Project/basics/int.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 func main(){
 	var a int //here data type will be int and initial value will be 0 singned integer can store -ve value
 	fmt.Println(a)
 	var b uint //here data type will be unsigned int and initial value will be 0 cannnot store -ve values
-	fmt.Println(b)
+	fmt.Println(b)//we can store 8,16,32,64 bit values
+	var c byte //here data type will be byte and initial value will be 0
+	fmt.Println(c,reflect.TypeOf(c))//we can store 8 bit values
+	c=100
+	fmt.Println(c,reflect.TypeOf(c))
 }
