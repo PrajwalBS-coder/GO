9d70f992d6bd909754ee7b669d42dccaa3d8ba45
Prajwal B S <prajwaljhon@gmail.com>
2025-07-07 21:05:22 +0530

    Add int.go with main function and variable declarations for int and uint types

---
+++ GO-Project/basics/int.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	var a int //here data type will be int and initial value will be 0 singned integer can store -ve value
+	fmt.Println(a)
+	var b uint //here data type will be unsigned int and initial value will be 0 cannnot store -ve values
+	fmt.Println(b)
+}
