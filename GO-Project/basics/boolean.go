2f0ffc97fe45cda6027b9180fb91b01e0ca2781e
PrajwalBS-coder <prajwaljhon@gmail.com>
2025-07-10 07:30:09 +0530

    Fix formatting in boolean.go to clarify output comments

--- GO-Project/basics/boolean.go
+++ GO-Project/basics/boolean.go
@@ -1,8 +1,10 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+)
 func main(){
 	b:=true //here data type will assingend based on value
 	var a bool //here data type will be boolean and initial value will be false
-	fmt.Println(b,a)
+	fmt.Println(b,a)//true false
 }
