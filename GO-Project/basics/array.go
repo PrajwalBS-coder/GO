bbfae9b8b2656e0ee1d770ff4491c9b0e1d39f11
Prajwal B S <prajwaljhon@gmail.com>
2025-07-11 08:23:08 +0530

    Add fixed-size array example and print type reflection in array.go

--- GO-Project/basics/array.go
+++ GO-Project/basics/array.go
@@ -5,8 +5,11 @@ import (
 	r "reflect"
 )
 func main(){
-	var array=[]int{1,2}
+	var array=[]int{1,2}//here data type will be []int here created variable using var keyword
 	f.Println(array,r.TypeOf(array))
-	a:=[]int{2,3}
+	a:=[]int{2,3} //here data type will be []int here created variable using := in oth cases array size is not fixed
 	f.Println(a,r.TypeOf(a))
+	b:=[3]int{1,2,3,4} //for fixed size array if we give more value it will show error .\array.go:12:18: index 3 is out of bounds (>= 3)
+	f.Println(b,r.TypeOf(b))
+
 }
