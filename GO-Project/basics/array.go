08768636ed1b53ed1e81951dda90edebdf6d66d5
Prajwal B S <prajwaljhon@gmail.com>
2025-07-16 20:29:12 +0530

    Fix: Correct fixed-size array initialization and add print statement for modified array

--- GO-Project/basics/array.go
+++ GO-Project/basics/array.go
@@ -9,6 +9,8 @@ func main(){
 	f.Println(array,r.TypeOf(array))
 	a:=[]int{2,3} //here data type will be []int here created variable using := in oth cases array size is not fixed
 	f.Println(a,r.TypeOf(a))
-	b:=[3]int{1,2,3,4} //for fixed size array if we give more value it will show error .\array.go:12:18: index 3 is out of bounds (>= 3)
+	b:=[3]int{1,2,3} //for fixed size array if we give more value it will show error .\array.go:12:18: index 3 is out of bounds (>= 3)
 	f.Println(b,r.TypeOf(b))
+	b[0]=100
+	f.Println(b)
 }
