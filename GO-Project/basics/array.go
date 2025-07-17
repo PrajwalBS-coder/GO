39b0070dabc468e9c68262305db274d9793c9541
Prajwal B S <prajwaljhon@gmail.com>
2025-07-17 22:02:55 +0530

    Changes in perticlau index

--- GO-Project/basics/array.go
+++ GO-Project/basics/array.go
@@ -11,6 +11,7 @@ func main(){
 	f.Println(a,r.TypeOf(a))
 	b:=[3]int{1,2,3} //for fixed size array if we give more value it will show error .\array.go:12:18: index 3 is out of bounds (>= 3)
 	f.Println(b,r.TypeOf(b))
-	b[0]=100
+	b[0]=100//Changing value sof perticular index
 	f.Println(b)
+
 }
