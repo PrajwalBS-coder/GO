0af21d0f481d3bac274a8ec788b3983dabb5b8d0
Prajwal B S <prajwaljhon@gmail.com>
2025-09-01 21:02:45 +0530

    Refactor array.go and slice.go: Improve formatting and add missing newlines for better readability

--- GO-Project/basics/slice.go
+++ GO-Project/basics/slice.go
@@ -16,4 +16,11 @@ func main(){
 	f.Println(s2,r.TypeOf(s2))
 	f.Println(len(s2),cap(s2))//here the length will be 4 and capacity will be 4
 
+
+	//Creting slice through array
+	s3:= []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10} //here data type will be []int here created variable using var keyword
+	f.Println(s3, r.TypeOf(s3))
+	p := s3[0:5] //here data type will be []int here created variable using var keyword
+	f.Println(p, r.TypeOf(p))
+
 }
