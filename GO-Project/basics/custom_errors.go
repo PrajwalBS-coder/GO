00ebe793fd7c77676155c162e78c232ea68dd246
PrajwalBS-coder <prajwaljhon@gmail.com>
2025-10-30 08:38:28 +0530

    docs: add comments explaining custom error types and their implementation

--- GO-Project/basics/custom_errors.go
+++ GO-Project/basics/custom_errors.go
@@ -33,3 +33,10 @@ func main() {
         fmt.Println("err doesn't match argError")
     }
 }
+// It’s possible to define custom error types by implementing the Error() method on them. 
+// Here’s a variant on the example above that uses a custom type to explicitly represent an argument error.
+// A custom error type usually has the suffix “Error”.
+// Adding this Error method makes argError implement the error interface.
+// errors.As is a more advanced version of errors.Is. 
+// It checks that a given error (or any error in its chain) matches a specific error type and converts to a value of that type, returning true. 
+// If there’s no match, it returns false.
